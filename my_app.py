4286325f59174479f345be551d01156fd7eaae98
HeatherLD <91164907+HeatherLD@users.noreply.github.com>
2022-02-22 21:03:17 -0600

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#pip install flask
+from flask import Flask, json, render_template, request
+import os
+
+#create instance of Flask app
+app = Flask(__name__)
+
+#decorator
+@app.route("/")
+def echo_hello():
+    return "Hello World!"
+
+@app.route("/all")
+def gdp():
+    json_url = os.path.join(app.static_folder,"","nobel.json")
+    data_json = json.load(open(json_url))
+
+    return render_template('index.html',data=data_json)
+
+@app.route("/all/<year>")
+def gdp_year(year):
+    json_url = os.path.join(app.static_folder,"","nobel.json")
+    data_json = json.load(open(json_url))
+
+    data = data_json[1]
+    #print(data)
+    year = request.view_args['year']
+
+    output_data = [x for x in data if x['year']==year]
+    return render_template('index.html',data=output_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
